4095620072a1e6de9d75f17334203e6c0396ad1c
davym <davymarthely@gmail.com>
2024-07-15 09:55:24 +0200

    feat(password):create all instances

--- src/main/java/com/example/passwordhandler/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.passwordhandler;
-
-import com.example.passwordhandler.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
-
